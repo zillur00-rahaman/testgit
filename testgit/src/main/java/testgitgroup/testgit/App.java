db2e1829b7d4f0fb1d5c86dc48c3c4df764dfa94
zillur00-rahaman <96796051+zillur00-rahaman@users.noreply.github.com>
2022-01-09 14:35:56 +0000

    Update App.java
    changes have been done in Github

--- testgit/src/main/java/testgitgroup/testgit/App.java
+++ testgit/src/main/java/testgitgroup/testgit/App.java
@@ -1,14 +1,12 @@
 package testgitgroup.testgit;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
+        
+        String name = "Zillur";
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello " +name+", this is changed from github");
         System.out.println("Successfully pushed to github");
         System.out.println("This is the second committ with first changes");
     }
