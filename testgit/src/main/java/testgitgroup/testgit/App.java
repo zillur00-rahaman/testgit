63a269fecfcbd78d04d942c5311e30b6e26a67ba
zillur00-rahaman <zillur00@yahoo.co.uk>
2022-01-09 14:24:51 +0000

    commit with first change

--- testgit/src/main/java/testgitgroup/testgit/App.java
+++ testgit/src/main/java/testgitgroup/testgit/App.java
@@ -9,5 +9,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println("Successfully pushed to github");
+        System.out.println("This is the second committ with first changes");
     }
 }
